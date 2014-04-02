d6fa29c4acf822712752e9b3d5f1fa9684d7baa2
rcburnett <rcburnett.bombardier@gmail.com>
2014-04-02 08:29:55 -0400

    Initialized repo

---
+++ BackboneDemo/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackboneDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult TemplateExample()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult ModelsExample()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CollectionsExample()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewsExample()
+        {
+            ViewBag.Message = "Views";
+
+            return View();
+        }
+
+        public ActionResult EventsExample()
+        {
+            ViewBag.Message = "Views";
+
+            return View();
+        }
+
+    }
+}
